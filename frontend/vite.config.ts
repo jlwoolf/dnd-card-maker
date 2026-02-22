b7e5aceb2b9470b3add74735ff4835f4034ff938
jonathan <jonathan@jlwoolf.com>
2026-02-22 13:21:39 -0700

    Made dev not use base url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/dnd-card-maker/",
+  base: process.env.NODE_ENV === "production" ? "/dnd-card-maker/" : undefined,
   plugins: [react()],
   resolve: {
     alias: {
